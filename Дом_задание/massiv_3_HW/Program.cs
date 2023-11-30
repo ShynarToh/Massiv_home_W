1c991753cb7af112011218a5595735a3dbb91cc2
Shynar <shurik_aaa@mail.ru>
2023-11-30 12:15:17 +0600

    3

---
+++ Дом_задание/massiv_3_HW/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+namespace massiv_3
+{
+    public class Program 
+    {
+        public static void Main (string [] arg){
+            Random rand = new Random();
+
+            int size = 100;
+            double [] array = new double [size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i]=rend.NextDouble() * (5.1 - 0.4) + 0.4;
+            }
+            foreach (double item in array)
+            {
+                System.Console.Write (item + " ");
+            }
+            
+            double maxDigit = 0;
+            double minDigit = 0;
+            
+            for (int i = 0; i <size; i++)
+            {
+                 if ( array [i] > maxDigit)   
+                     {
+                       maxDigit = array [i];
+                       i++;                      
+                     }    
+                     if ( array [i] < minDigit)   
+                     {
+                       minDigit = array [i];
+                       i++;                      
+                     }                      
+            }
+            double result = maxDigit - minDigit;
+             Console.Write($" => {result}");
+             
+        }
+
+    }
+}
